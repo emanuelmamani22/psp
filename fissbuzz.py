cd5aaba7f112bb01d51cbba898660727dbb88636
Nestor Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-06-01 15:36:29 -0300

    pseudocodigo

--- fissbuzz.py
+++ fissbuzz.py
@@ -1,1 +1,8 @@
 """Programa que imprime Fizz si un numero es multiplo de 3, imprime Buzz si es Multiplo de 5 y fizzbuzz si es multiplo de 3 y 5"""
+
+#pseudocodigo:
+#iniciar una variable llamada "a" en 1
+#while mientras que a sea menor a 101:
+#dividir el numero por 5 y 3 si es TRUE imprimir fizzbuzz
+#si es divisible por 5 y da 0 imprimir BUZZ
+#si es divisible por 3 y da 0 imprimir FIZZ
